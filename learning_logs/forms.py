3b3bb005df5dbd40615e4c8063b4a9f7828beac8
RecursiveBlade <2410338657@qq.com>
2026-09-13 16:02:55 +0800

    Add new entry form

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic          # 基于 Topic 模型创建表单
         fields = ['text']      # 表单中只包含 text 字段
         labels = {'text': ''}  # 告诉 Django 不要为 text 字段生成标签（显示更简洁）
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}  # 指定 text 字段使用 Textarea 小部件，并设置列数为 80
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry          # 基于 Entry 模型创建表单
+        fields = ['text']      # 表单中只包含 text 字段
+        labels = {'text': ''}  # 告诉 Django 不要为 text 字段生成标签（显示更简洁）
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}  # 指定 text 字段使用 Textarea 小部件，并设置列数为 80
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic, Entry  # <--- 新增导入
-from .forms import TopicForm  # <--- 新增导入
+from .forms import TopicForm,EntryForm  # <--- 新增导入
 
 def index(request):
     """学习笔记的主页"""
@@ -33,3 +33,21 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+def new_entry(request, topic_id):
+    """在特定的主题中添加新条目"""    
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据：创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
